cabc22397aeafa9dd24ed169bc47a8df3bcdbaca
Don Law <don@salientsoftware.net>
2015-03-06 23:49:19 -0500

    Reformat controllers for 4 columns. Refactor method roleText in user model. Get rid of extra flash notice.

--- app/controllers/mockup_controller.rb
+++ app/controllers/mockup_controller.rb
@@ -1,13 +1,13 @@
 class MockupController < ApplicationController
-  def internal_veh
-  end
+    def internal_veh
+    end
 
-  def multi_veh
-  end
+    def multi_veh
+    end
 
-  def customer_veh
-  end
+    def customer_veh
+    end
 
-  def store
-  end
+    def store
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,29 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+    # Include default devise modules. Others available are:
+    # :token_authenticatable, :confirmable,
+    # :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  attr_accessible :first_name, :last_name, :role
+    # Setup accessible (or protected) attributes for your model
+    attr_accessible :email, :password, :password_confirmation, :remember_me
+    attr_accessible :first_name, :last_name, :role
 
-  # See vpatient if you need to hook user creation: before_create :pre_create
+    # See vpatient if you need to hook user creation: before_create :pre_create
 
-  ROLE_ADMIN = 11
-  ROLE_CLERK = 12
-  ROLE_MOCKER = 13
+    ROLE_ADMIN = 11
+    ROLE_CLERK = 12
+    ROLE_MOCKER = 13
+    ALL_ROLES = [ ROLE_ADMIN, ROLE_CLERK, ROLE_MOCKER, ]
 
 
-  def toText()
-      return 'Administrator'   if role == ROLE_ADMIN
-      return 'Clerk'           if role == ROLE_CLERK
-      return 'Mockup Viewer'   if role == ROLE_MOCKER
-      return 'Unknown'
-  end
+    def roleText(r = nil)
+        r = role  if r.nil?
+        return 'empty'           if r.nil?
+        return 'Administrator'   if r == ROLE_ADMIN
+        return 'Clerk'           if r == ROLE_CLERK
+        return 'Mockup Viewer'   if r == ROLE_MOCKER
+        return 'Unknown'
+    end
 
 end
