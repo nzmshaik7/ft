4d22ae51503b80f3c68e6d74a1fc6f277b6162c6
Don Law <don@salientsoftware.net>
2015-03-12 23:30:33 -0400

    Add new animated FT logo.

---
+++ app/assets/javascripts/ftlogo.js
@@ -0,0 +1,168 @@
+function bodyloaded() { 
+    if (document.images) {
+        cube06 = new Image(800,240);
+        cube06.src="/assets/logoanim/ffout006.jpg";
+        cube07 = new Image(800,240);
+        cube07.src="/assets/logoanim/ffout007.jpg";
+        cube08 = new Image(800,240);
+        cube08.src="/assets/logoanim/ffout008.jpg";
+        cube09 = new Image(800,240);
+        cube09.src="/assets/logoanim/ffout009.jpg";
+        cube10 = new Image(800,240);
+        cube10.src="/assets/logoanim/ffout010.jpg";
+        cube11 = new Image(800,240);
+        cube11.src="/assets/logoanim/ffout011.jpg";
+        cube12 = new Image(800,240);
+        cube12.src="/assets/logoanim/ffout012.jpg";
+        cube13 = new Image(800,240);
+        cube13.src="/assets/logoanim/ffout013.jpg";
+        cube14 = new Image(800,240);
+        cube14.src="/assets/logoanim/ffout014.jpg";
+        cube15 = new Image(800,240);
+        cube15.src="/assets/logoanim/ffout015.jpg";
+        cube16 = new Image(800,240);
+        cube16.src="/assets/logoanim/ffout016.jpg";
+        cube17 = new Image(800,240);
+        cube17.src="/assets/logoanim/ffout017.jpg";
+        cube18 = new Image(800,240);
+        cube18.src="/assets/logoanim/ffout018.jpg";
+        cube19 = new Image(800,240);
+        cube19.src="/assets/logoanim/ffout019.jpg";
+        cube20 = new Image(800,240);
+        cube20.src="/assets/logoanim/ffout020.jpg";
+        cube21 = new Image(800,240);
+        cube21.src="/assets/logoanim/ffout021.jpg";
+        cube22 = new Image(800,240);
+        cube22.src="/assets/logoanim/ffout022.jpg";
+        cube23 = new Image(800,240);
+        cube23.src="/assets/logoanim/ffout023.jpg";
+        cube24 = new Image(800,240);
+        cube24.src="/assets/logoanim/ffout024.jpg";
+        cube25 = new Image(800,240);
+        cube25.src="/assets/logoanim/ffout025.jpg";
+        cube26 = new Image(800,240);
+        cube26.src="/assets/logoanim/ffout026.jpg";
+        cube27 = new Image(800,240);
+        cube27.src="/assets/logoanim/ffout027.jpg";
+        cube28 = new Image(800,240);
+        cube28.src="/assets/logoanim/ffout028.jpg";
+        cube29 = new Image(800,240);
+        cube29.src="/assets/logoanim/ffout029.jpg";
+        cube30 = new Image(800,240);
+        cube30.src="/assets/logoanim/ffout030.jpg";
+        cube31 = new Image(800,240);
+        cube31.src="/assets/logoanim/ffout031.jpg";
+        cube32 = new Image(800,240);
+        cube32.src="/assets/logoanim/ffout032.jpg";
+        cube33 = new Image(800,240);
+        cube33.src="/assets/logoanim/ffout033.jpg";
+        cube34 = new Image(800,240);
+        cube34.src="/assets/logoanim/ffout034.jpg";
+        cube35 = new Image(800,240);
+        cube35.src="/assets/logoanim/ffout035.jpg";
+        cube36 = new Image(800,240);
+        cube36.src="/assets/logoanim/ffout036.jpg";
+        cube37 = new Image(800,240);
+        cube37.src="/assets/logoanim/ffout037.jpg";
+        cube38 = new Image(800,240);
+        cube38.src="/assets/logoanim/ffout038.jpg";
+        cube39 = new Image(800,240);
+        cube39.src="/assets/logoanim/ffout039.jpg";
+        cube40 = new Image(800,240);
+        cube40.src="/assets/logoanim/ffout040.jpg";
+        cube41 = new Image(800,240);
+        cube41.src="/assets/logoanim/ffout041.jpg";
+        cube42 = new Image(800,240);
+        cube42.src="/assets/logoanim/ffout042.jpg";
+        cube43 = new Image(800,240);
+        cube43.src="/assets/logoanim/ffout043.jpg";
+        cube44 = new Image(800,240);
+        cube44.src="/assets/logoanim/ffout044.jpg";
+        cube45 = new Image(800,240);
+        cube45.src="/assets/logoanim/ffout045.jpg";
+        cube46 = new Image(800,240);
+        cube46.src="/assets/logoanim/ffout046.jpg";
+        cube47 = new Image(800,240);
+        cube47.src="/assets/logoanim/ffout047.jpg";
+        cube48 = new Image(800,240);
+        cube48.src="/assets/logoanim/ffout048.jpg";
+        cube49 = new Image(800,240);
+        cube49.src="/assets/logoanim/ffout049.jpg";
+        cube50 = new Image(800,240);
+        cube50.src="/assets/logoanim/ffout050.jpg";
+        cube51 = new Image(800,240);
+        cube51.src="/assets/logoanim/ffout051.jpg";
+        cube52 = new Image(800,240);
+        cube52.src="/assets/logoanim/ffout052.jpg";
+        cube53 = new Image(800,240);
+        cube53.src="/assets/logoanim/ffout053.jpg";
+        cube54 = new Image(800,240);
+        cube54.src="/assets/logoanim/ffout054.jpg";
+        cube55 = new Image(800,240);
+        cube55.src="/assets/logoanim/ffout055.jpg";
+        seq = 6;
+	if (cube06.complete) {
+	    setTimeout("NextCube()", 200);
+	}
+    }
+}
+ 
+function NextCube() {
+    if (seq == 6) { imgOn="/assets/logoanim/ffout006.jpg"; }
+    if (seq == 7) { imgOn="/assets/logoanim/ffout007.jpg"; }
+    if (seq == 8) { imgOn="/assets/logoanim/ffout008.jpg"; }
+    if (seq == 9) { imgOn="/assets/logoanim/ffout009.jpg"; }
+    if (seq == 10) { imgOn="/assets/logoanim/ffout010.jpg"; }
+    if (seq == 11) { imgOn="/assets/logoanim/ffout011.jpg"; }
+    if (seq == 12) { imgOn="/assets/logoanim/ffout012.jpg"; }
+    if (seq == 13) { imgOn="/assets/logoanim/ffout013.jpg"; }
+    if (seq == 14) { imgOn="/assets/logoanim/ffout014.jpg"; }
+    if (seq == 15) { imgOn="/assets/logoanim/ffout015.jpg"; }
+    if (seq == 16) { imgOn="/assets/logoanim/ffout016.jpg"; }
+    if (seq == 17) { imgOn="/assets/logoanim/ffout017.jpg"; }
+    if (seq == 18) { imgOn="/assets/logoanim/ffout018.jpg"; }
+    if (seq == 19) { imgOn="/assets/logoanim/ffout019.jpg"; }
+    if (seq == 20) { imgOn="/assets/logoanim/ffout020.jpg"; }
+    if (seq == 21) { imgOn="/assets/logoanim/ffout021.jpg"; }
+    if (seq == 22) { imgOn="/assets/logoanim/ffout022.jpg"; }
+    if (seq == 23) { imgOn="/assets/logoanim/ffout023.jpg"; }
+    if (seq == 24) { imgOn="/assets/logoanim/ffout024.jpg"; }
+    if (seq == 25) { imgOn="/assets/logoanim/ffout025.jpg"; }
+    if (seq == 26) { imgOn="/assets/logoanim/ffout026.jpg"; }
+    if (seq == 27) { imgOn="/assets/logoanim/ffout027.jpg"; }
+    if (seq == 28) { imgOn="/assets/logoanim/ffout028.jpg"; }
+    if (seq == 29) { imgOn="/assets/logoanim/ffout029.jpg"; }
+    if (seq == 30) { imgOn="/assets/logoanim/ffout030.jpg"; }
+    if (seq == 31) { imgOn="/assets/logoanim/ffout031.jpg"; }
+    if (seq == 32) { imgOn="/assets/logoanim/ffout032.jpg"; }
+    if (seq == 33) { imgOn="/assets/logoanim/ffout033.jpg"; }
+    if (seq == 34) { imgOn="/assets/logoanim/ffout034.jpg"; }
+    if (seq == 35) { imgOn="/assets/logoanim/ffout035.jpg"; }
+    if (seq == 36) { imgOn="/assets/logoanim/ffout036.jpg"; }
+    if (seq == 37) { imgOn="/assets/logoanim/ffout037.jpg"; }
+    if (seq == 38) { imgOn="/assets/logoanim/ffout038.jpg"; }
+    if (seq == 39) { imgOn="/assets/logoanim/ffout039.jpg"; }
+    if (seq == 40) { imgOn="/assets/logoanim/ffout040.jpg"; }
+    if (seq == 41) { imgOn="/assets/logoanim/ffout041.jpg"; }
+    if (seq == 42) { imgOn="/assets/logoanim/ffout042.jpg"; }
+    if (seq == 43) { imgOn="/assets/logoanim/ffout043.jpg"; }
+    if (seq == 44) { imgOn="/assets/logoanim/ffout044.jpg"; }
+    if (seq == 45) { imgOn="/assets/logoanim/ffout045.jpg"; }
+    if (seq == 46) { imgOn="/assets/logoanim/ffout046.jpg"; }
+    if (seq == 47) { imgOn="/assets/logoanim/ffout047.jpg"; }
+    if (seq == 48) { imgOn="/assets/logoanim/ffout048.jpg"; }
+    if (seq == 49) { imgOn="/assets/logoanim/ffout049.jpg"; }
+    if (seq == 50) { imgOn="/assets/logoanim/ffout050.jpg"; }
+    if (seq == 51) { imgOn="/assets/logoanim/ffout051.jpg"; }
+    if (seq == 52) { imgOn="/assets/logoanim/ffout052.jpg"; }
+    if (seq == 53) { imgOn="/assets/logoanim/ffout053.jpg"; }
+    if (seq == 54) { imgOn="/assets/logoanim/ffout054.jpg"; }
+    if (seq == 55) { imgOn="/assets/logoanim/ffout055.jpg"; }
+    document["mobi"].src= imgOn;
+
+    seq = seq + 1;
+    if (seq > 55) {
+	seq = 6;
+    }
+    setTimeout("NextCube()", 70);
+} 
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,7 @@
 class StaticController < ApplicationController
 
     skip_before_filter :authenticate_user!
+    layout "anim", :only => [:home]
 
     def home
     end
