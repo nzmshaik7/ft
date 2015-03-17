5fab672c0f8af1c2b9272f3afed4d2e3a213bed6
Don Law <don@salientsoftware.net>
2015-03-16 23:26:46 -0400

    Configure the asset pipeline. Sort of figured out how to conditionally include javascript and stylesheets.  Yah! Laid in styelsheets and javascript needed by mockup.

---
+++ app/assets/javascripts/mockup/mockup.js
@@ -0,0 +1,68 @@
+$(function () { 
+    // enable all popovers on page.
+    $("[data-toggle='popover']").popover();
+
+    // Toggle view/hide button.  This allows the popover to be dismissed
+    // either by clicking Hide or clicking anywhere.
+    //
+    $('.show_hide_popover').on('shown.bs.popover', function () {
+      // If the popover is show, remove the View button and enable Hide.
+      var myId = jQuery(this).attr("id");
+      var hideId = '#x' + myId;
+      jQuery(this).css("display", "none");
+      $(hideId).css("display", "block");
+      // alert('shown');
+    });
+    $('.show_hide_popover').on('hidden.bs.popover', function () {
+      // Opposite of the above.
+      // var myId = jQuery(this).attr("id");
+      // var showId = '#' + myId.substring(1);
+      // jQuery(this).css("display", "none");
+      // $(showId).css("display", "block");
+      var myId = jQuery(this).attr("id");
+      var hideId = '#x' + myId;
+      jQuery(this).css("display", "block");
+      $(hideId).css("display", "none");
+    });
+
+});
+
+$(document).ready(function(){
+
+  // Connect the popover to its content in a non-visible div.
+  //
+  $('#vr1id').popover({ 
+    html : true, content: function() { return $('#vr1id_div').html(); }
+  });
+  $('#vr2id').popover({ 
+    html : true, content: function() { return $('#vr2id_div').html(); }
+  });
+  $('#vr3id').popover({ 
+    html : true, content: function() { return $('#vr3id_div').html(); }
+  });
+  $('#vr4id').popover({ 
+    html : true, content: function() { return $('#vr4id_div').html(); }
+  });
+  $('#vr5id').popover({ 
+    html : true, content: function() { return $('#vr5id_div').html(); }
+  });
+  $('#vr6id').popover({ 
+    html : true, content: function() { return $('#vr6id_div').html(); }
+  });
+  $('#cust1id').popover({ 
+    html : true, content: function() { return $('#cust1id_div').html(); }
+  });
+  $('#cust2id').popover({ 
+    html : true, content: function() { return $('#cust2id_div').html(); }
+  });
+  $('#vehphot').popover({ 
+    html : true, content: function() { return $('#vehphot_div').html(); }
+  });
+  $('#doorphot').popover({ 
+    html : true, content: function() { return $('#doorphot_div').html(); }
+  });
+  $('#titlphot').popover({ 
+    html : true, content: function() { return $('#titlphot_div').html(); }
+  });
+
+});
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ Ft::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( bootstrap-popbase.css mcommon.css mock.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
