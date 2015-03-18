73ba4ea608951a5523ac9243b9b5ee5c93641278
Don Law <don@salientsoftware.net>
2015-03-17 23:16:00 -0400

    Basically have internal mockup page working.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Ft::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( bootstrap-popbase.css mcommon.css mock.css )
+  config.assets.precompile += %w( bootstrap-popbase.css mcommon.css mock.css unoverride.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
