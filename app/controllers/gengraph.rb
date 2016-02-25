ff7992cfd4c9ce640beb4d7814272f1e47641f65
Don Law <don@salientsoftware.net>
2016-02-25 18:37:07 -0500

    Lay groundwork for graphics generation.

---
+++ app/controllers/gengraph.rb
@@ -0,0 +1,62 @@
+class Gengraph
+
+    require 'socket'
+
+    @@GRAPHDIR = "/home/rails/ft/public/graphs"
+    @@HOST = 'localhost'
+    @@PORT = 7001
+
+
+    def initialize()
+        @graphUrls = Hash.new
+    end
+
+
+    def requestPlot?(name, imageFile, data)
+        begin
+            s = TCPSocket.open(@@HOST, @@PORT)
+            s.write "#{name},#{imageFile}\n"
+            s.write data
+            s.shutdown(Socket::SHUT_WR)
+            resp = s.read
+            if resp[0..1] == 'OK'
+                return true
+            else
+                return false
+            end
+        rescue
+            return false
+        end
+         
+    end
+
+
+    def clubfeespie(veh, membClubFees, membScheduled, membUnscheduled)
+
+        # scheduled,unscheduled,gross 
+        imgfile = "clubfeespie-#{veh.id}.png"
+        clubfeespie = "#{@@GRAPHDIR}/#{imgfile}"
+        grossProfit = membClubFees - (membScheduled + membUnscheduled)
+
+        if grossProfit < 0
+            grossProfit = 0 - grossProfit 
+            data = "#{membScheduled},#{membUnscheduled},#{grossProfit}\n"
+            worked = requestPlot?('clubfeesloss', clubfeespie, data)
+        else
+            data = "#{membScheduled},#{membUnscheduled},#{grossProfit}\n"
+            worked = requestPlot?('clubfeesprof', clubfeespie, data)
+        end
+        if worked
+            @graphUrls['clubfeespie'] = "/graphs/#{imgfile}"
+        else
+            @graphUrls['clubfeespie'] = "/assets/gen/ploterr.png"
+        end
+
+    end
+
+
+    def getUrls
+        return @graphUrls
+    end
+
+end
